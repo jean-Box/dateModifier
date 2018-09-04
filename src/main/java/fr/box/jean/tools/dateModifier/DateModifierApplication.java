04f2d13e4320c948c7e146ef2ca499dcade46c33
LAFOSSAS <david.lafossas@capgemini.com>
2018-09-04 21:52:37 +0200

    init project

---
+++ src/main/java/fr/box/jean/tools/dateModifier/DateModifierApplication.java
@@ -0,0 +1,12 @@
+package fr.box.jean.tools.dateModifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateModifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateModifierApplication.class, args);
+	}
+}
